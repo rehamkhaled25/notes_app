ff32d025469504496f26a794b2d997da2e459594
rehamkhaled25 <rerekg75@gmail.com>
2025-08-26 17:45:37 +0300

    create notes state

--- lib/cubits/add_note_state.dart
+++ lib/cubits/add_note_state.dart
@@ -1,8 +1,14 @@
-class AddNoteState {}
-class AddNoteInitial extends AddNoteState{}
-class AddNoteLoading extends AddNoteState{}
-class AddNoteSuccess extends AddNoteState{}
-class AddNoteFailure extends AddNoteState{
+import 'package:last_project/models/note_model.dart';
+
+class NotesState {}
+class NotesInitial extends NotesState{}
+class NotesLoading extends NotesState{}
+class NotesSuccess extends NotesState{
+final List<NoteModel> notes;
+
+  NotesSuccess(this.notes);
+}
+class NotesFailure extends NotesState{
   final String errMessage;
-  AddNoteFailure(this.errMessage);
+  NotesFailure(this.errMessage);
 }
